f25c88765247e2033fbc3a7eba6cd2c80a35bdd8
Mahia Islam <72724016+MahiaIslam@users.noreply.github.com>
2020-11-14 20:20:41 +0600

    Create Profile.php

---
+++ Lab Task 4.2 FILE/View/Profile.php
@@ -0,0 +1,45 @@
+<?php
+	
+	session_start();
+    if(isset($_REQUEST['submit'])){
+        if(!empty($_REQUEST['name']) and !empty($_REQUEST['password'])){
+            
+            $info = $_REQUEST['name']."|".$_REQUEST['password']."\n";
+            $myfile = fopen("register.txt", "w");
+            fwrite($myfile, $info);
+            fclose($myfile);
+            echo "Registration done!";
+            
+        }
+        
+        else{
+            echo "Error!";
+            
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile page</title>
+</head>
+<body>
+
+	<h3>Profile Page</h3>
+	
+	<a href="home.php">Back</a> |
+	<a href="../php/logout.php">logout</a>
+	<br><br>
+	
+	<img src="../assets/<?=$_SESSION['pic']?>" width="100px" height="100px">
+	<br>
+	
+	<form method="post" action="profile.php">
+		Register Name: <input type="text" name="name"><br>
+		Register Password <input type="password" name="password"><br>
+		<input type="submit" name="submit" value="Submit">
+		
+	</form>
+</body>
+</html>
